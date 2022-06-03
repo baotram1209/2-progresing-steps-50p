95adaf0b6102dde43b98f6fa2d102f994d9cdce7
baotram1209 <baotramtran.1209@gmail.com>
2022-06-03 15:46:04 +0200

    finish

--- script1.js
+++ script1.js
@@ -6,13 +6,13 @@ const circles = document.querySelectorAll('.circle');
 let currentActive = 0;
 next.addEventListener('click', () => {
   currentActive++;
+
   if (currentActive >= circles.length) {
     currentActive = circles.length - 1;
   }
   console.log(currentActive);
   update();
 });
-
 prev.addEventListener('click', () => {
   currentActive--;
   if (currentActive < 0) {
@@ -21,27 +21,23 @@ prev.addEventListener('click', () => {
   update();
 });
 
-// Hàm update lại toàn bộ các circle mỗi lần click
 function update() {
   circles.forEach((circle, i) => {
-    if (i <= currentActive) {
+    if (currentActive >= i) {
       circle.classList.add('active');
     } else {
       circle.classList.remove('active');
     }
   });
-
-  //Tạo progress bar chạy màu
   const actives = document.querySelectorAll('.active');
   progress.style.width =
     ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
-  //Bật tắt nút "Prev" & Next
-  if (currentActive === 0) {
-    prev.disabled = true;
-  } else if (currentActive === circles.length - 1) {
+  if (currentActive === circles.length - 1) {
     next.disabled = true;
+  } else if (currentActive === 0) {
+    prev.disabled = true;
   } else {
-    prev.disabled = false;
     next.disabled = false;
+    prev.disabled = false;
   }
 }
